d3daaa8e2b3fcddacef835f47a66afb498dcd4e2
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-11-09 12:02:51 +0300

    Merged files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
 
-const App = () => {
-  return (
-    <div>App</div>
+import './App.css'; 
+import React from 'react';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
+import Products from './components/Products';
+import {Routes,Route} from 'react-router-dom';
+
+
+function App() {
+  return ( 
+    <>
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/products" element={<Products/>}/>
+    </Routes>
+    </>
   )
 }
 
-export default App
+export default App;
